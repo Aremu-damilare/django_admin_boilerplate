4d078ec3699b770481fb0539cb680cef8cb0ee79
aremu-damilare <a.matthew2013@gmail.com>
2025-03-03 14:05:44 +0100

    Django Management Command to Copy the Entire admin boilerplate App

--- django_admin_boilerplate/apps.py
+++ django_admin_boilerplate/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
+class DjangoAdminBoilerplateConfig(AppConfig):
+    name = 'django_admin_boilerplate'
 
-class AdminBoilerplateConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin_boilerplate'
+    def ready(self):
+        import django_admin_boilerplate.management.commands
